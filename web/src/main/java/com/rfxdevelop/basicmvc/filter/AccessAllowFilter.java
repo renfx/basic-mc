4f46d0a8e2491a4c6737ead33b6ba3fcdf44993f
renfx@wealthlake.com.cn <renfx@wealthlake.com.cn>
2018-09-12 16:33:12 +0800

    SQL注入检查

--- web/src/main/java/com/rfxdevelop/basicmvc/filter/AccessAllowFilter.java
+++ web/src/main/java/com/rfxdevelop/basicmvc/filter/AccessAllowFilter.java
@@ -1,11 +1,17 @@
 package com.rfxdevelop.basicmvc.filter;
 
+import com.renfxdevelop.utils.copy.sql.SqlCheckUtils;
+import com.renfxdevelop.utils.original.request.RequestParamUtil;
 import org.springframework.core.annotation.Order;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.Set;
 
 @Order(0)
 @WebFilter(filterName = "AccessAllowFilter",urlPatterns = {"/basic/*","/baseData/*","/simple/*"})
@@ -19,7 +25,18 @@ public class AccessAllowFilter implements Filter{
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        //接受跨域请求
         response.setHeader("Access-Control-Allow-Origin","*");
+        Map<String, String> params = RequestParamUtil.getParams(servletRequest);
+        Set<String> keySet = params.keySet();
+        //SQL注入检查
+        for(String key:keySet){
+            String value = params.get(key);
+            if(!SqlCheckUtils.isValid(value)){
+                throw new ServletException("参数错误：key:"+key+" value:"+ value );
+            }
+        }
         filterChain.doFilter(servletRequest,servletResponse);
     }
 
